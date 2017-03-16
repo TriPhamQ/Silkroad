7e3261870db6ce0d074b6576e94eb08efa4db5bb
Tri Pham <phamqtri1992@gmail.com>
2017-03-15 17:42:55 -0700

    Add item to card and Shopping cart working
    If no user logged in and add to cart -> redirect to log in and if log in
    success will automaticly add to card

--- apps/customer_app/urls.py
+++ apps/customer_app/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^browse/(?P<category_name>[\w\-]+)/(?P<current_page>\d+)$', views.browse, name = 'browse'),
-    url(r'^user$', views.user, name = 'user')
+    url(r'^user$', views.user, name = 'user'),
+    url(r'^product/(?P<product_id>\d+)$', views.product_page, name = 'product_page'),
+    url(r'^add-to-cart/(?P<product_id>\d+)$', views.add_to_cart, name = 'add_to_cart')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/customer_app/views.py
+++ apps/customer_app/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import render, redirect, reverse
-from .models import Category, Product, Image, Cart, Order, OrderProduct, BillingAddress, ShippingAddress
+from .models import User, Category, Product, Image, Cart, Order, OrderProduct, BillingAddress, ShippingAddress
 import os, math
 
 # Create your views here.
 def index(request):
+    # Use this next line to clear cart while test
+    # Cart.objects.all().delete()
     if 'logged_user' not in request.session:
         logged_user = 0
     else:
         logged_user = request.session['logged_user']
-    # print(logged_user)
-    categories = Category.objects.all()
-    products = Product.objects.filter(ongoing = True).order_by('category')
-    images = Image.objects.all()
-    context = {
-        'logged_user': logged_user,
-        'categories': categories,
-        'products': products,
-        'images': images
-    }
+    try:
+        user = User.objects.get(id = logged_user)
+    except:
+        user = None
+    if user:
+        cart = Cart.objects.filter(user = user)
+        cart_item = 0
+        for item in range (0, cart.count()):
+            cart_item += cart[item].quantity
+        categories = Category.objects.all()
+        products = Product.objects.filter(ongoing = True).order_by('category')
+        images = Image.objects.all()
+        context = {
+            'logged_user': logged_user,
+            'cart': cart,
+            'cart_item': cart_item,
+            'categories': categories,
+            'products': products,
+            'images': images
+        }
+    else:
+        categories = Category.objects.all()
+        products = Product.objects.filter(ongoing = True).order_by('category')
+        images = Image.objects.all()
+        context = {
+            'logged_user': logged_user,
+            'categories': categories,
+            'products': products,
+            'images': images
+        }
     return render(request, 'index.html', context)
 
 def browse(request, category_name, current_page):
@@ -25,7 +47,6 @@ def browse(request, category_name, current_page):
         logged_user = 0
     else:
         logged_user = request.session['logged_user']
-    # print(logged_user)
     category = Category.objects.get(name = category_name)
     products = Product.objects.filter(category = category)
     page_size = 15
@@ -38,6 +59,70 @@ def browse(request, category_name, current_page):
         'products': products_out
     }
     return render(request, 'browse.html', context)
+
+def product_page(request, product_id):
+    if 'logged_user' not in request.session:
+        logged_user = 0
+    else:
+        logged_user = request.session['logged_user']
+    try:
+        user = User.objects.get(id = logged_user)
+    except:
+        user = None
+    product = Product.objects.get(id = product_id)
+    if user:
+        cart = Cart.objects.filter(user = user)
+        cart_item = 0
+        for item in range (0, cart.count()):
+            cart_item += cart[item].quantity
+        categories = Category.objects.all()
+        products = Product.objects.filter(ongoing = True).order_by('category')
+        images = Image.objects.all()
+        context = {
+            'product': product,
+            'logged_user': logged_user,
+            'cart': cart,
+            'cart_item': cart_item,
+            'categories': categories,
+            'products': products,
+            'images': images
+        }
+    else:
+        categories = Category.objects.all()
+        products = Product.objects.filter(ongoing = True).order_by('category')
+        images = Image.objects.all()
+        context = {
+            'product': product,
+            'logged_user': logged_user,
+            'categories': categories,
+            'products': products,
+            'images': images
+        }
+    return render(request, 'product-page.html', context)
+
+def add_to_cart(request, product_id):
+    if 'logged_user' not in request.session:
+        form = request.POST
+        request.session['product_id'] = product_id
+        request.session['product_quantity'] = form['product_quantity']
+        return redirect('/login_registration')
+    else:
+        logged_user = request.session['logged_user']
+        product = Product.objects.get(id = product_id)
+        form = request.POST
+        if int(form['product_quantity']) <= product.inventory:
+            user = User.objects.get(id = logged_user)
+            try:
+                item = Cart.objects.get(user = user, product = product)
+            except:
+                item = None
+            if item:
+                item.quantity += int(form['product_quantity'])
+                item.save()
+            else:
+                item = Cart.objects.create(quantity = form['product_quantity'], user = user, product = product)
+        return redirect('/')
+    return redirect('/')
 
 def user(request):
     if 'logged_user' not in request.session:
